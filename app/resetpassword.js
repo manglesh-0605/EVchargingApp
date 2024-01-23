094633ae41959078893943ab09a2e78d4fa9215a
Manglesh <manglesh0605@gmail.com>
2024-01-24 00:41:27 +0530

    added reset password screen modal

--- app/resetpassword.js
+++ app/resetpassword.js
@@ -1,22 +1,31 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Modal, Pressable } from 'react-native';
+
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { router } from 'expo-router'
-import InputField from '../src/components/InputField';
 import { AntDesign } from '@expo/vector-icons';
+import InputField from '../src/components/InputField';
 import ThemeConstant from '../src/constants/ThemeConstant';
 import CustomButton from '../src/components/CustomButton';
 import BackButton from '../src/components/BackButton';
+import { moderateScale, moderateVerticalScale } from 'react-native-size-matters';
+import { StatusBar } from 'expo-status-bar';
 
 
 const ResetPassword = () => {
     const [password, setPasseord] = useState('');
     const [confirmPassword, setConrimPassword] = useState('');
+    const [showModal, setShowModal] = useState(false);
 
     const handleLogin = () => {
         // Implement your login logic here
+        setShowModal(true)
 
     };
+
+    const toggleModal = () => {
+        setShowModal(!showModal);
+    }
 
 
 
@@ -56,6 +65,30 @@ const ResetPassword = () => {
             </View>
             <CustomButton title="Set Password" onPress={handleLogin} />
 
+            {
+                showModal &&
+                <Modal
+                    animationType='fade'
+                    transparent={true}
+                    visible={showModal}
+                    onRequestClose={() => {
+                        setShowModal(!showModal);
+                    }}>
+                    <StatusBar hidden={true} />
+                    <TouchableOpacity activeOpacity={1} onPress={toggleModal} style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <View style={styles.check}>
+                                <AntDesign name="check" size={50} color="#fff" />
+                            </View>
+                            <Text style={styles.modaltitle}>Password Changed
+                                Successfully!</Text>
+                            <Text style={styles.modalsubtitle}>Your Password is changed successfully.
+                                Please login again to continue</Text>
+                        </View>
+                    </TouchableOpacity>
+                </Modal>
+            }
+
 
 
         </SafeAreaView >
@@ -77,22 +110,55 @@ const styles = StyleSheet.create({
         color: ThemeConstant.FADED_BLACK,
         marginBottom: 52
     },
-    forgotpasswordText: {
-        color: ThemeConstant.FADED_BLACK,
-        textAlign: 'right',
-        marginTop: 5,
-        marginBottom: 40
+
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: moderateScale(22),
+        paddingHorizontal: moderateScale(28),
+        backgroundColor: 'rgba(0,0,0,0.8)'
     },
-    or: {
+    modalView: {
+        width: '100%',
+        height: moderateVerticalScale(300),
+        backgroundColor: 'white',
+        borderRadius: 10,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modaltitle: {
+        fontSize: 24,
+        fontWeight: '700',
+        marginBottom: 15,
+        textAlign: 'center',
+        marginVertical: 20
+    },
+    check: {
+        width: moderateScale(80),
+        height: moderateScale(80),
+        borderRadius: 80,
+        backgroundColor: ThemeConstant.PRIMARY_COLOR,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    modalsubtitle: {
         color: ThemeConstant.FADED_BLACK,
         textAlign: 'center',
-        margin: 8
-    },
-    signup: {
-        fontWeight: 'bold',
-    },
-    signupbox: {
-
+        fontSize: 14
     }
 });
 
